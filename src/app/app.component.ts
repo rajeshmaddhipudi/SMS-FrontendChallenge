89890e879fa098f6ba24fc1c08614ff476587361
unknown <rajeshmaddhipudi@gmail.com>
2019-02-22 13:10:22 +0100

    - imported all header fields as a table as requirement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'SMS-FrontendChallenge';
-}
+    constructor(private httpService: HttpClient) { }
+    arrData: string [];
+
+    ngOnInit() {
+      this.httpService.get('./assets/data.json').subscribe(
+        data => {
+          this.arrData = data as string [];	 // FILL THE ARRAY WITH DATA.
+          //  console.log(this.arrBirds[1]);
+        },
+        (err: HttpErrorResponse) => {
+          console.log (err.message);
+        }
+      );
+    }
+  }
+
